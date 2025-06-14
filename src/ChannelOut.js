c206a5b48cfde0a99d4fa3ae4829ec0537e59a6e
tommy martinez <thomasjohnmartinez@gmail.com>
2025-06-14 00:51:59 -0400

    clean up variable names and comments in ChannelOut

--- src/ChannelOut.js
+++ src/ChannelOut.js
@@ -1,29 +1,28 @@
-//TODO: this is a stub for AudioOut
 class ChannelOut {
     constructor(channel = 0, context) {
         this.context = context || getAudioContext();
         this.channel = channel;
-        this.audioSource = this.context.createGain();
+        this.input = this.context.createGain();
         let maxChannelCount = this.context.destination.maxChannelCount;
         this.context.destination.channelCount = maxChannelCount;
         this.merger = this.context.createChannelMerger(maxChannelCount);
-        this.audioSource.channelCount = 1;
-        this.audioSource.channelCountMode = 'explicit';
-        this.audioSource.channelInterpretation = 'discrete';
-        this.audioSource.connect(this.merger, 0, channel);
+        this.input.channelCount = 1;
+        this.input.channelCountMode = 'explicit';
+        this.input.channelInterpretation = 'discrete';
+        this.input.connect(this.merger, 0, channel);
         this.merger.connect(this.context.destination);
     }
 
     //switch the channel to a different one
     switch(channel) {
         this.channel = channel;
-        this.audioSource.disconnect();
-        this.audioSource.connect(this.merger, 0, channel);
+        this.input.disconnect();
+        this.input.connect(this.merger, 0, channel);
     }
 
     //get the audio source node
     getNode() {
-        return this.audioSource;
+        return this.input;
     }
 }
 
