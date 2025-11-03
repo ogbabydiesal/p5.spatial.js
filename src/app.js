a978f12aacc47bc7c7ded9d782055c67c807eada
tommy martinez <thomasjohnmartinez@gmail.com>
2025-11-02 23:10:44 -0500

    removed unused imports and definitions in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
 import ChannelOut from './ChannelOut.js';
 import AudioSource from './AudioSource';
-import { quadraphonic, octophonic, fivePointOne } from './SpeakerLayouts.js';
-
-
-p5.prototype.quadraphonic = quadraphonic;
-p5.prototype.octophonic = octophonic;
-p5.prototype.fivePointOne = fivePointOne;
 
 p5.ChannelOut = ChannelOut;
 p5.AudioSource = AudioSource;
